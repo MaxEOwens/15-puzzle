de98c37580290e44a9d19bc94fc8b93c5adf543b
MaxEOwens <maxwell.e.owens@gmail.com>
2025-05-12 05:52:27 -0700

    Initial Commit

---
+++ SlidingPuzzle/SlidingPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingPuzzleApp.swift
+//  SlidingPuzzle
+//
+//  Created by Max Owens on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SlidingPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
